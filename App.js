f9d446b0e05b469dab64fb2de6d6aa6381f1f9f0
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2023-01-15 22:29:35 +0100

    get familiar with text

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, View } from 'react-native';
 
 export default function App() {
+	const handleOnPress = () => console.log('text pressed');
 	return (
 		<View style={styles.container}>
-			<Text>Hello React App!</Text>
+			<Text numberOfLines={1} onPress={handleOnPress}>
+				Used to truncate the text with an ellipsis after computing the text
+				layout, including line wrapping, such that the total number of lines
+				does not exceed this number. Setting this property to 0 will result in
+				unsetting this value, which means that no lines restriction will be
+				applied.
+			</Text>
 			<StatusBar style="auto" />
 		</View>
 	);
@@ -13,7 +20,7 @@ export default function App() {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: '#fff',
+		backgroundColor: 'dodgerblue',
 		alignItems: 'center',
 		justifyContent: 'center',
 	},
